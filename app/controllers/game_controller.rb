126984fdd299d744776d08268804d4a83138fa8f
Martin Carniello <Martin Carniello@gmail.com>
2015-01-07 19:38:59 -0300

    Página principal + Parcial BreadCrumb y Rating + Estilos

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,7 @@
 class GameController < ApplicationController
   def index
+    @page_number = params[:pageNumber].to_i
+    @game_category = params[:gameCategory]
     game_name = params[:game_name]
     @game = Game.where(name: game_name).first
   end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,6 @@ class IndexController < ApplicationController
   def index
     @game_category = !params[:gameCategory] ? "all" : params[:gameCategory]
     @page_number = !params[:pageNumber] ? 1 : params[:pageNumber].to_i
-    @query_string_parameters = "&gameCategory=#{@game_category}"
     @games = Game.games_page(@game_category, @page_number)
     @game_quantity = Game.game_quantity(@game_category)
 
